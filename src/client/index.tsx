1a8788904baf58b5f2db72ed2835fd48c0f12c77
kk-mats <k.mats.kk@gmail.com>
2019-10-09 15:10:13 +0900

    Prepare styling with material-ui

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,5 +1,14 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { CssBaseline } from "@material-ui/core";
+import { ThemeProvider } from "@material-ui/styles";
+import theme from "./styles/theme";
 import CloneViewer from "./components/CloneViewer";
 
-ReactDOM.render(<CloneViewer />, document.getElementById("root"));
+ReactDOM.render(
+	<ThemeProvider theme={theme}>
+		<CssBaseline />
+		<CloneViewer />
+	</ThemeProvider>,
+	document.getElementById("root")
+);
