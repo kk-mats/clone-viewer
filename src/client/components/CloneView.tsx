3a35eb40dff16999ed79fec60fe93ad00a6c634d
kk-mats <k.mats.kk@gmail.com>
2019-10-09 15:14:45 +0900

    Fix routing when result selected or reloading

--- src/client/components/CloneView.tsx
+++ src/client/components/CloneView.tsx
@@ -1,14 +1,89 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { RouteComponentProps } from "react-router-dom";
+import { Switch, Route, RouteComponentProps } from "react-router-dom";
+import Axios from "axios";
+import { CircularProgress, useTheme } from "@material-ui/core";
+import { History } from "history";
 
-import CloneList from "./CloneList";
+import Navigator from "./Navigator";
+import ResultSelect from "./ResultSelect";
+import ResultProperty from "./ResultProperty";
+import ResultSelectResponse from "../../models/ResultSelectResponse";
+import Provider, { CloneContext } from "../states/store";
+import useStyles from "../styles/styles";
 
-type Props = RouteComponentProps<{ filename: string; id: string }>;
+type WrapperProps = RouteComponentProps<{
+	filename: string;
+}>;
+
+export const UrlWrapper: React.FunctionComponent<WrapperProps> = (
+	props: WrapperProps
+) => {
+	const { match, history } = props;
+	const { filename } = match.params;
+	return (
+		<Provider filename={filename}>
+			<CloneView history={history} />
+		</Provider>
+	);
+};
+
+type Props = {
+	history: History;
+};
 
 const CloneView: React.FunctionComponent<Props> = (props: Props) => {
-	const { filename, id } = props.match.params;
-	return <CloneList filename={filename} id={id} />;
+	const { history } = props;
+	const { state, dispatch } = React.useContext(CloneContext);
+	const { filename } = state;
+	const [results, setResults] = React.useState<ResultSelectResponse | null>(
+		null
+	);
+
+	const classes = useStyles(useTheme);
+
+	React.useEffect(() => {
+		Axios.get(`/api/view/${filename}`).then(value => {
+			const data = value.data as ResultSelectResponse;
+			setResults(data);
+		});
+	}, []);
+
+	React.useEffect(() => {
+		if (state.result) {
+			history.push(`/view/${filename}/${state.result}`);
+		}
+	}, [state.result]);
+
+	if (!results) {
+		return <CircularProgress />;
+	}
+
+	const names: string[] = [];
+	results.results.forEach(value => {
+		names.push(value.environment.name);
+	});
+
+	return (
+		<div className={classes.root}>
+			<Navigator />
+			<main className={classes.content}>
+				<ResultSelect resultNames={names} />
+				<Switch>
+					<Route
+						path={`/view/${filename}/:result`}
+						component={ResultProperty}
+					/>
+					<Route
+						path={`/view/${filename}`}
+						component={() => {
+							return <p> statistics </p>;
+						}}
+					/>
+				</Switch>
+			</main>
+		</div>
+	);
 };
 
 export default CloneView;
--- src/client/components/CloneViewer.tsx
+++ src/client/components/CloneViewer.tsx
@@ -1,15 +1,21 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { BrowserRouter, Route, Link } from "react-router-dom";
+
 import CloneFileSelector from "./CloneFileSelector";
-import CloneView from "./CloneView";
+import { UrlWrapper } from "./CloneView";
 
 const CloneViewer: React.FunctionComponent = () => {
 	return (
 		<BrowserRouter>
 			<div>
 				<Route exact path="/" component={CloneFileSelector} />
-				<Route path="/view/:filename" component={CloneView} />
+				<Route path="/view/:filename" component={UrlWrapper} />
+				<Route
+					component={() => {
+						return <p>missing</p>;
+					}}
+				/>
 			</div>
 		</BrowserRouter>
 	);
